de92fa9dc130785252838597874c7d606c4d10ed
3782291211 <Laklouk98@hotmail.com>
2022-12-29 01:04:59 +0000

    Updating component function with new hook; updated JSX template

--- src/components/AddSong.jsx
+++ src/components/AddSong.jsx
@@ -7,20 +7,28 @@ export function AddSong ({setPlaylist}) {
     const [newArtist, setArtist] = useState('');
     const [imgUrl, setImgUrl] = useState('');
     const [songInfo, setSongInfo] = useState({title: "", album: "", artist: "", genre: "" , artwork: ""});
+    const [search, setSearch] = useState('false');
+    const [error, setError] = useState('');
    
     const handleNewSong = e => setSong(e.target.value);
     const handleNewArtist = e => setArtist(e.target.value);
-    console.log(songInfo);
+    
     useEffect(() => {
-      if (newSong && newArtist) {
+    //  if (newSong.length > 1 && newArtist.length > 1) {
         musicInfo.searchSong(
-          {title: newSong, artist: newArtist}, 1000).then(obj => {
-            setImgUrl(obj.artwork);
-            setSongInfo(() => ({title: obj.title, album: obj.album, artist: obj.artist, genre: obj.genre, artwork: obj.artwork }));
+          {title: newSong, artist: newArtist}).then(obj => {
+            if (obj.artwork) {
+              setImgUrl(obj.artwork);
+              setSongInfo(() => {
+                return {title: obj.title, album: obj.album, artist: obj.artist, genre: obj.genre, artwork: obj.artwork};
+            });
+            } else {
+               setSearch(prev => !prev);
+               setError('error');
+            }
           }).catch(() => {});
-        setImgUrl(false);
-      }
-    }, [newSong, newArtist]);
+     // }
+    }, [search]);
     
     const handleSubmit = e => {
       e.preventDefault();
@@ -35,23 +43,33 @@ export function AddSong ({setPlaylist}) {
       });
       } 
     };
-    
+    console.log(error);
+    console.log(imgUrl);
       return (
         <div>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={e => {
+            e.preventDefault();
+            setImgUrl('');
+            setError('clicked');
+            setSearch(prev => !prev);
+          }}>
     
           <label htmlFor="new-artist">Enter artist name</label>
-          <input id="new-artist" type="text" placeholder="Artist name goes here..." value={newArtist} onChange={handleNewArtist}></input><br/>
+          <input id="new-artist" type="text" placeholder="Artist name" value={newArtist} onChange={handleNewArtist}></input><br/>
     
           <label htmlFor="new-song">Enter song name</label>
-          <input id="new-song" type="text" placeholder="Song name goes here..." value={newSong} onChange={handleNewSong}></input><br/>
-    
-          <button id="add">Add to playlist</button>
+          <input id="new-song" type="text" placeholder="Song name" value={newSong} onChange={handleNewSong}></input><br/>
+          <button id="preview" type="submit">Preview track details</button>
+          </form>
+
+          <form id="form2" onSubmit={handleSubmit}>
+          <button id="add" typ="submit">Add to playlist</button>
         </form>
-        {!imgUrl && newSong && newArtist ? <p id="error"> Unable to retrieve track details. Please refine your search.</p> : null}
-        {imgUrl && newSong && newArtist ? <AlbumArt imgUrl={imgUrl} song={songInfo} /> : null}
+        {!imgUrl && error === 'clicked' ? 
+            <p id="error"> Unable to retrieve track details. Please refine your search.</p> 
+            : imgUrl ? 
+            <AlbumArt imgUrl={imgUrl} song={songInfo}/> : null}
         </div>
       )
     };
-    
-    
+  
